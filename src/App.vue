838a4cc1492c79e8727d245dc0d1b5e4f24a8272
Giulio998 <giulio_favia@hotmail.it>
2024-04-16 16:32:00 +0200

    Milestone 2

--- src/App.vue
+++ src/App.vue
@@ -12,12 +12,15 @@ methods: {
   getTitle(){
       axios.get(store.movieEndPoint + store.query + "&api_key=" + store.api_key).then((res) => {
       store.movies = res.data.results;  
-      console.log(store.movies);
       
     })  
-  }
-},
 
+    axios.get(store.tvEndPoint + store.query + "&api_key=" + store.api_key).then((res) => {
+      store.tvs = res.data.results;  
+      
+  })
+},
+}
 };
 
 </script>
@@ -27,22 +30,33 @@ methods: {
 <input v-model="store.query" type="text">
 <button @click="getTitle()">Cerca</button>
  <ul>
+  <h1>Film</h1>
   <template v-for="(movie, index) in store.movies" :key="index">
-
-
     <li >
     <h2>Titolo: {{ movie.title }}</h2>
     <h2>Titolo originale: {{ movie.original_title }}</h2>
-    <h2>Lingua originale: {{ movie.original_language }}</h2>
+   <img v-if="original_language == 'it' | original_language == 'en' | original_language == 'de' | original_language =='es' | original_language == 'fr'" class="flag" :src="'src/assets/'+ movie.original_language + '.png' " alt="" srcset="">
+   <h2 v-else>{{ movie.original_language.toUpperCase() }}</h2>
     <h2>Voto: {{ movie.vote_average }}</h2>
   </li>
-      
-    
   </template>
-  
+</ul> 
+<ul>
+  <h1>Serie tv</h1>
+  <template v-for="(tv, index) in store.tvs" :key="index">
+    <li >
+    <h2>Titolo: {{ tv.name }}</h2>
+    <h2>Titolo originale: {{ tv.original_name }}</h2>
+    <img v-if="original_language == 'it' | original_language == 'en' | original_language == 'de' | original_language =='es' | original_language == 'fr'" class="flag" :src="'src/assets/'+ movie.original_language + '.png' " alt="" srcset="">
+    <h2 v-else>{{ tv.original_language.toUpperCase() }}</h2>
+    <h2>Voto: {{ tv.vote_average }}</h2>
+  </li>
+  </template>
 </ul> 
 </template>
 
 <style scoped>
-
+.flag{
+  height: 20px;
+}
 </style>
--- store.js
+++ store.js
@@ -3,8 +3,9 @@ import { reactive } from "vue";
 export const store = reactive({
         api_key: "221ddfa5fda3be6026dfe32e023f2d36",
         movies: [],
+        tvs: [],
         query: "",
-        movieEndPoint: `https://api.themoviedb.org/3/search/movie?query=`
+        movieEndPoint: `https://api.themoviedb.org/3/search/movie?query=`,
+        tvEndPoint: `https://api.themoviedb.org/3/search/tv?query=`,
 
-  
 });
